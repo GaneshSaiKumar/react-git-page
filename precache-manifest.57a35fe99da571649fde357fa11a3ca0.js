6c58465233023b329b914ed054e60528676b499c
ganesh97 <ganeshk@sigaramtech.com>
2019-07-13 15:59:47 +0530

    Updates

---
+++ precache-manifest.57a35fe99da571649fde357fa11a3ca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f96a9703486ceff74f28dd22737b251",
+    "url": "/GaneshSaiKumar/react-git-page/index.html"
+  },
+  {
+    "revision": "60c04ec27ece8eb3ded9",
+    "url": "/GaneshSaiKumar/react-git-page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/GaneshSaiKumar/react-git-page/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "60c04ec27ece8eb3ded9",
+    "url": "/GaneshSaiKumar/react-git-page/static/js/main.daf9028f.chunk.js"
+  },
+  {
+    "revision": "e7df7a2a916874492922",
+    "url": "/GaneshSaiKumar/react-git-page/static/js/runtime~main.eb4f5e84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GaneshSaiKumar/react-git-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.764f61ec8c91f473dab1e83f0e820778.js"
+  "/GaneshSaiKumar/react-git-page/precache-manifest.57a35fe99da571649fde357fa11a3ca0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GaneshSaiKumar/react-git-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
