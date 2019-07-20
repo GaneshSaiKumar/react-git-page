d4c90ed39215965777e2d8be9fbf251e43c741fd
ganesh97 <ganeshk@sigaramtech.com>
2019-07-20 14:56:58 +0530

    inp

--- src/App.js
+++ src/App.js
@@ -6,19 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          First me and my self in react
-          {/* <code>src/App.js</code> and save to reload. */}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <p> Eyes </p>
       </header>
     </div>
   );
